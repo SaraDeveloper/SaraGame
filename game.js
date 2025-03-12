7d18cffe1982edb34a4833102d54f720e9c34ae6
Hadi <sararobati12@gmail.com>
2025-03-11 21:47:46 -0700

    Adjust collision detection with padding for more forgiving hitboxes

--- game.js
+++ game.js
@@ -423,10 +423,12 @@ function gameLoop() {
 
 // Collision detection
 function checkCollision(rect1, rect2) {
-    return rect1.x < rect2.x + rect2.width &&
-           rect1.x + rect1.width > rect2.x &&
-           rect1.y < rect2.y + rect2.height &&
-           rect1.y + rect1.height > rect2.y;
+    // Add padding to make collision less sensitive
+    const padding = 15; // pixels of padding around the hitbox
+    return (rect1.x + padding) < (rect2.x + rect2.width - padding) &&
+           (rect1.x + rect1.width - padding) > (rect2.x + padding) &&
+           (rect1.y + padding) < (rect2.y + rect2.height - padding) &&
+           (rect1.y + rect1.height - padding) > (rect2.y + padding);
 }
 
 // Handle keyboard input
